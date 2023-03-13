7530b5011326667536ed5b42272d8b880708d9c1
rplkita <satriayudha01@gmail.com>
2023-03-13 16:17:20 +0700

    lengkap

--- asset/js/main.js
+++ asset/js/main.js
@@ -42,3 +42,24 @@ window.onscroll = () => {
 
 
 };
+
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal ('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal ('.home-img, .services-container, .portfolio-box, .contact-form', {origin: 'buttom'});
+ScrollReveal().reveal ('.home-content h1, .about-img' , { origin: 'left' });
+ScrollReveal().reveal ('.home-content p, .about-content' , { origin: 'right' });
+
+// type js
+const typed = new Typed ('.multiple-text', {
+    strings: ['Frontend Developer','YouTuber', 'Contect Creator'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
